f62ca84a095efb18519005306eb07f0e9c51d879
Colin Nielsen <colinrnielsen@gmail.com>
2023-03-08 10:27:27 -0700

    setup foundry

--- test/Kevin.t.sol
+++ test/Kevin.t.sol
@@ -1,35 +1,35 @@
-pragma solidity ^0.8.15;
+// pragma solidity ^0.8.15;
 
-import "forge-std/Test.sol";
-import "forge-std/console2.sol";
+// import "forge-std/Test.sol";
+// import "forge-std/console2.sol";
 
-import {Kevin} from "contracts/mocks/Kevin.sol";
+// import {Kevin} from "contracts/mocks/Kevin.sol";
 
-contract KevinTest is Test {
+// contract KevinTest is Test {
 
-    Kevin kevin;
+//     Kevin kevin;
 
-    function setUp() external {
-        kevin = new Kevin();
-    }
+//     function setUp() external {
+//         kevin = new Kevin();
+//     }
 
-    function getBasicProofRequest() public pure returns (string[] memory) {
-        string[] memory inputs = new string[](7);
-        inputs[0] = "npx";
-        inputs[1] = "ts-node";
-        inputs[2] = "test/utils/ffiProof.ts";
-        return inputs;
-    }
+//     function getBasicProofRequest() public pure returns (string[] memory) {
+//         string[] memory inputs = new string[](7);
+//         inputs[0] = "npx";
+//         inputs[1] = "ts-node";
+//         inputs[2] = "test/utils/ffiProof.ts";
+//         return inputs;
+//     }
 
-    function testLabouji() external {
-        string[] memory inputs = getBasicProofRequest();
-        inputs[3] = "p";
-        inputs[4] = "1";
-        inputs[5] = "2";
-        inputs[6] = "3";
-        bytes memory proof = vm.ffi(inputs);
-        console2.logBytes(proof);
-        kevin.set(proof);
-        assertEq(kevin.last(), 3);
-    }
-}
+//     function testLabouji() external {
+//         string[] memory inputs = getBasicProofRequest();
+//         inputs[3] = "p";
+//         inputs[4] = "1";
+//         inputs[5] = "2";
+//         inputs[6] = "3";
+//         bytes memory proof = vm.ffi(inputs);
+//         console2.logBytes(proof);
+//         kevin.set(proof);
+//         assertEq(kevin.last(), 3);
+//     }
+// }
