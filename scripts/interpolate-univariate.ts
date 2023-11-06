59c96a71b47b37b8440b74387005cf379501abc9
Colin Nielsen <colinrnielsen@gmail.com>
2023-11-06 15:21:06 +0100

    fix: update polynomial args

--- scripts/interpolate-univariate.ts
+++ scripts/interpolate-univariate.ts
@@ -3,6 +3,10 @@ import { writeFileSync } from "fs";
 import { hashMessage } from "viem";
 import { PrivateKeyAccount, privateKeyToAccount } from "viem/accounts";
 
+const MAX_SIGNERS = 8;
+const SIGNER_COUNT = process.argv[2] ? +process.argv[2] : null;
+const THRESHOLD = process.argv[3] ? process.argv[3] : null;
+
 const bn_254_fp =
   21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 
@@ -22,68 +26,76 @@ const DEFAULT_SIGNERS_PK = [
 
 const DEFAULT_SIGNERS = DEFAULT_SIGNERS_PK.map((pk) => privateKeyToAccount(pk));
 
-const MAX_SIGNERS = DEFAULT_SIGNERS.length;
-
 async function main() {
-  const SIGNERS: PrivateKeyAccount[] = await inquirer
-    .prompt({
-      message: "Select signers",
-      name: "signers",
-      type: "checkbox",
-      choices: [
-        ...DEFAULT_SIGNERS.map((account) => ({
-          value: account,
-          name: account.address,
-        })),
-        new inquirer.Separator(),
-      ],
-    })
-    .then(({ signers }: { signers: bigint[] }) => {
-      if (signers.length < 2)
-        throw new Error("Must select at least two signers");
-      return signers;
-    });
+  const signers: PrivateKeyAccount[] = SIGNER_COUNT
+    ? DEFAULT_SIGNERS.slice(0, SIGNER_COUNT)
+    : await inquirer
+        .prompt({
+          message: "Select signers",
+          name: "signers",
+          type: "checkbox",
+          choices: [
+            ...DEFAULT_SIGNERS.map((account) => ({
+              value: account,
+              name: account.address,
+            })),
+            new inquirer.Separator(),
+          ],
+        })
+        .then(({ signers }: { signers: bigint[] }) => {
+          if (signers.length < 2)
+            throw new Error("Must select at least two signers");
+          return signers;
+        });
 
-  const threshold = await inquirer
-    .prompt({
-      message: "Enter a signing threshold",
-      name: "threshold",
-      type: "number",
-    })
-    .then(({ threshold }: { threshold: number }) => {
-      if (threshold == 0) throw new Error("Threshold must be greater than 0");
-      if (threshold > SIGNERS.length)
-        throw new Error("Threshold is greater than signer count");
+  const threshold = THRESHOLD
+    ? THRESHOLD
+    : await inquirer
+        .prompt({
+          message: "Enter a signing threshold",
+          name: "threshold",
+          type: "number",
+        })
+        .then(({ threshold }: { threshold: number }) => {
+          if (threshold == 0)
+            throw new Error("Threshold must be greater than 0");
+          if (threshold > signers.length)
+            throw new Error("Threshold is greater than signer count");
 
-      return threshold;
-    });
+          return threshold;
+        });
 
   console.log("Given a Threshold of: ", threshold);
   console.log(
     "And the following of signers: ",
-    SIGNERS.map(({ address }) => address)
+    signers.map(({ address }) => address)
   );
 
-  const combinations = generateCombinations(
-    SIGNERS.map(({ address }) => BigInt(address)),
+  const combinations = kChooseN(
+    signers.map(({ address }) => BigInt(address)),
     threshold
   );
-  console.log("Yields the combinations: ", combinations);
+  console.log(
+    "Yields the combinations: ",
+    combinations.map((c) => `0x` + c.toString(16))
+  );
 
-  const _P = computePolynomial(combinations);
+  const _P = rootsToPolynomial(combinations);
   // pad the polynomial to 70 coefficients
-  const P = _P.concat(new Array(100).fill(0n)).slice(0, 70);
+  const P = _P.concat(new Array(100).fill(0n)).slice(0, 71);
+
   console.log(
     "And the polynomial",
-    P.map((p) => p.toString(10))
+    P.map((p) => p.toString(16))
   );
   console.log(
     `Evaluating polynomial P of degree ${P.length} \n================`
   );
 
   combinations.forEach((combo, i) => {
+    console.log(`combo: 0x${combo.toString(16)}`);
     const result = evauluatePolynomial(P, combo);
-    console.log({ x: combo.toString(10), result });
+    console.log({ x: combo.toString(16), result });
     if (result === 0n)
       console.log("Evaluation of combo @ index: " + i, " = " + result);
     else
@@ -103,7 +115,7 @@ async function main() {
   });
 
   const pubKeyAndSigners = await Promise.all(
-    SIGNERS.map(async (account) => {
+    signers.slice(0, threshold).map(async (account) => {
       const pubKey = account.publicKey.slice(4); // remove 0x04 prefix
       const fullSig = await account.signMessage({ message: MESSAGE });
 
@@ -118,11 +130,14 @@ async function main() {
     })
   );
   const pubKeyAndSignersWithEmpty = pubKeyAndSigners.concat(
-    emptyPubKeyAndSigners.slice(SIGNERS.length)
+    emptyPubKeyAndSigners.slice(pubKeyAndSigners.length, MAX_SIGNERS)
   );
+
   writeFileSync(
     "circuits/Prover.toml",
-    `polynomial = [${P.map((p) => `"${p.toString(10)}"`)}]\n` +
+    `polynomial = [\n${
+      P.map((p) => `"0x${p.toString(16)}"`).join(",\n") + "\n"
+    }]\n` +
       `r = 0\n` +
       `polynomial_commitment = 0\n` +
       `safe_message_hash = [${hexToUint8Array(MESSAGE_HASH)}]\n` +
@@ -147,56 +162,56 @@ main().catch(console.error);
 //// utils
 //
 
-function generateCombinations(array: bigint[], k: number): bigint[] {
-  if (k > array.length || k <= 0) return [];
-  if (k === array.length) return [array.reduce((a, b) => a + b, 0n)];
-  if (k === 1) return array;
+function kChooseN(k: bigint[], n: number): bigint[] {
+  if (n > k.length || n <= 0) return [];
+  if (n === k.length) return [k.reduce((a, b) => a + b, 0n)];
+  if (n === 1) return k;
 
   let result: bigint[] = [];
-  for (let i = 0; i < array.length; i++) {
-    const smallerCombinations = generateCombinations(array.slice(i + 1), k - 1);
-    for (let j = 0; j < smallerCombinations.length; j++) {
-      result.push(array[i] + smallerCombinations[j]);
-    }
+  for (let i = 0; i < k.length; i++) {
+    const smallerCombinations = kChooseN(k.slice(i + 1), n - 1);
+
+    for (let j = 0; j < smallerCombinations.length; j++)
+      result.push(k[i] + smallerCombinations[j]);
   }
 
   return result;
 }
 
-// Multiply two polynomials
-function multiplyPolynomials(p1: bigint[], p2: bigint[]): bigint[] {
-  const result: bigint[] = new Array(p1.length + p2.length - 1).fill(0n);
+function rootsToPolynomial(roots: bigint[]): bigint[] {
+  let coefficients: bigint[] = [1n]; // Start with a polynomial of degree 0: f(x) = 1
 
-  for (let i = 0; i < p1.length; i++) {
-    for (let j = 0; j < p2.length; j++) {
-      result[i + j] =
-        (result[i + j] + (((p1[i] * p2[j]) % bn_254_fp) % bn_254_fp)) %
+  for (let i = 0; i < roots.length; i++) {
+    let root = roots[i];
+    let newCoefficients: bigint[] = new Array(coefficients.length + 1).fill(0n);
+    newCoefficients[0] = (coefficients[0] * -root) % bn_254_fp;
+
+    for (let j = 1; j < coefficients.length; j++) {
+      newCoefficients[j] =
+        (((coefficients[j] * -root) % bn_254_fp) +
+          (coefficients[j - 1] % bn_254_fp)) %
         bn_254_fp;
     }
+
+    newCoefficients[coefficients.length] =
+      coefficients[coefficients.length - 1];
+    coefficients = newCoefficients;
   }
 
-  return result;
-}
-
-// Given an array of roots, compute the coefficients of the polynomial
-// TODO: need to hash and addmod f_p
-function computePolynomial(roots: bigint[]): bigint[] {
-  const polynomial = roots.reduce(
-    (poly, root) => multiplyPolynomials(poly, [-root, 1n]),
-    [1n]
+  // if there are negative coefficients, normalize them as the field representation
+  return coefficients.map((c) =>
+    c < 0 ? c + bn_254_fp : c > bn_254_fp ? c - bn_254_fp : c
   );
-
-  return polynomial.map((p) => (p < 0n ? p + bn_254_fp : p));
 }
 
 // Evaluate the polynomial at x
 function evauluatePolynomial(P: bigint[], x: bigint) {
-  return P.reduce((acc, coefficient, degree) => {
-    return (
-      (acc += (coefficient * (x ** BigInt(degree) % bn_254_fp)) % bn_254_fp) %
-      bn_254_fp
-    );
-  }, 0n);
+  return P.reduce(
+    (acc, coefficient, degree) =>
+      (acc + ((coefficient * (x ** BigInt(degree) % bn_254_fp)) % bn_254_fp)) %
+      bn_254_fp,
+    0n
+  );
 }
 
 const hexToUint8Array = (hex: string) =>
